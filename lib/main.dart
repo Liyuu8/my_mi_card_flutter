25d90f22bded4b481e7ae751b77a280a81d6379a
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-03-14 21:37:55 +0900

    Add my icon and custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/profile-icon.png'),
+                ),
+                Text(
+                  'Liyuu',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'DancingScript',
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
