ef7fa58695414add910dc08fa41eb91af365de49
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-03-20 02:03:24 +0900

    Add ListTile Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,119 +11,97 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/profile-icon.png'),
-                ),
-                Text(
-                  'Liyuu',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'DancingScript',
-                  ),
-                ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.white70,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'SourceSansPro',
-                    letterSpacing: 2.5,
-                  ),
-                ),
-                Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile-icon.png'),
+              ),
+              Text(
+                'Liyuu',
+                style: TextStyle(
+                  fontSize: 40.0,
                   color: Colors.white,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'DancingScript',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white70,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.phone,
-                          color: Colors.teal,
-                        ),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text(
-                          '+44 123 456 789',
-                          style: TextStyle(
-                              color: Colors.teal.shade900,
-                              fontFamily: 'SourceSansPro',
-                              fontSize: 20.0),
-                        )
-                      ],
-                    ),
+                  title: Text(
+                    '+44 123 456 789',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0),
                   ),
                 ),
-                Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.email,
-                          color: Colors.teal,
-                        ),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text(
-                          'user@test.com',
-                          style: TextStyle(
-                              color: Colors.teal.shade900,
-                              fontFamily: 'SourceSansPro',
-                              fontSize: 20.0),
-                        )
-                      ],
-                    ),
+                  title: Text(
+                    'user@test.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0),
                   ),
                 ),
-                Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.vpn_key,
+                    color: Colors.teal,
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.vpn_key,
-                          color: Colors.teal,
-                        ),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text(
-                          '*************',
-                          style: TextStyle(
-                              color: Colors.teal.shade900,
-                              fontFamily: 'SourceSansPro',
-                              fontSize: 20.0),
-                        )
-                      ],
-                    ),
+                  title: Text(
+                    '*************',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
